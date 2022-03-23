60cbfb91b7c4b264ba0387d2d94688f6136cb94f
Krystian <krystian14d@gmail.com>
2022-03-23 19:48:25 +0100

    added non-standard login configuration

---
+++ src/main/java/pl/javastart/sec/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package pl.javastart.sec.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(requests -> requests.anyRequest().authenticated());
+        http.csrf().disable();
+        http.formLogin(login -> login.loginPage("/login").permitAll());
+    }
+}
