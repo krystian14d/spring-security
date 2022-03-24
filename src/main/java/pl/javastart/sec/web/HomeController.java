ba1d76a6872a134100a59e49580d29aa1398104a
Krystian <krystian14d@gmail.com>
2022-03-24 19:43:33 +0100

    safe password storage - lesson

--- src/main/java/pl/javastart/sec/web/HomeController.java
+++ src/main/java/pl/javastart/sec/web/HomeController.java
@@ -1,13 +1,25 @@
 package pl.javastart.sec.web;
 
+import org.springframework.security.core.annotation.CurrentSecurityContext;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import pl.javastart.sec.message.WelcomeMessageService;
 
 @Controller
 public class HomeController {
 
+    private WelcomeMessageService welcomeMessageService;
+
+    public HomeController(WelcomeMessageService welcomeMessageService) {
+        this.welcomeMessageService = welcomeMessageService;
+    }
+
     @GetMapping("/")
-    String home(){
+    String home(@RequestParam(defaultValue = "en") String lang, Model model) {
+        String welcomeMessage = welcomeMessageService.getWelcomeMessage(lang);
+        model.addAttribute("welcomeMessage", welcomeMessage);
         return "index";
     }
 }
