a4a7bf1b3fe87d7e5c92b1b51b6b920cac6eadf6
Krystian <krystian14d@gmail.com>
2022-03-24 18:54:08 +0100

    added default users source - admin and user

--- src/main/java/pl/javastart/sec/config/SecurityConfig.java
+++ src/main/java/pl/javastart/sec/config/SecurityConfig.java
@@ -1,8 +1,13 @@
 package pl.javastart.sec.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -12,5 +17,13 @@ class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests(requests -> requests.anyRequest().authenticated());
         http.csrf().disable();
         http.formLogin(login -> login.loginPage("/login").permitAll());
+    }
+
+    @Bean
+    public UserDetailsService customUserDetailsService() {
+        User.UserBuilder userBuilder = User.builder();
+        UserDetails admin = userBuilder.username("admin").password("{noop}admin").roles("ADMIN").build();
+        UserDetails user1 = userBuilder.username("user").password("{noop}user").roles("USER").build();
+        return new InMemoryUserDetailsManager(admin, user1);
     }
 }
